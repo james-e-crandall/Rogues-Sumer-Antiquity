8efa6e6dc37301be63eaf8b1fa860e302f12edff
james-e-crandall <james.e.crandall@gmail.com>
2025-07-05 22:34:17 -0500

    initial commit, compiles

---
+++ ui/game.js
@@ -0,0 +1,17 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_WAR_CHARIOT_JEC': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UUNIT_WAR_CHARIOT_JEC_2': { //your unit type
+        unitType: 'UNIT_CHARIOT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }
+}
+
+
+
+
+
+
